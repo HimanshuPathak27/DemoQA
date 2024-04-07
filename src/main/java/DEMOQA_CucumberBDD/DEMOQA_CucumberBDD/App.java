d5219f46e9523aca3c859f62c903c9abcf6a5de0
HimanshuPathak27 <himanshupathak2910@gmail.com>
2024-04-07 20:06:21 +0530

    First Commit - New Maven Project created

---
+++ src/main/java/DEMOQA_CucumberBDD/DEMOQA_CucumberBDD/App.java
@@ -0,0 +1,13 @@
+package DEMOQA_CucumberBDD.DEMOQA_CucumberBDD;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/DEMOQA_CucumberBDD/DEMOQA_CucumberBDD/AppTest.java
@@ -0,0 +1,20 @@
+package DEMOQA_CucumberBDD.DEMOQA_CucumberBDD;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
